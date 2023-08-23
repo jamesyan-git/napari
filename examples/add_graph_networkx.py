93d353ad8656390bfbf910d2f2ac27dd22eea73a
jamesyan-git <work.jdryan@gmail.com>
2023-08-23 15:52:23 +1000

    Make grid a bit bigger to avoid weird vis issues

--- examples/add_graph_networkx.py
+++ examples/add_graph_networkx.py
@@ -14,12 +14,23 @@ import numpy as np
 import napari
 
 hex_grid = nx.hexagonal_lattice_graph(5, 5, with_positions=True)
+# make grid a bit bigger
+nx.set_node_attributes(
+    hex_grid,
+    {
+        k: (10 * v[0], 10 * v[1])
+        for k, v in nx.get_node_attributes(hex_grid, 'pos').items()
+    },
+    'pos',
+)
 # below conversion not needed after napari/napari-graph#11 is released
 hex_grid_ints = nx.convert_node_labels_to_integers(hex_grid)
 
 viewer = napari.Viewer()
-layer = viewer.add_graph(hex_grid_ints, size=1)
-layer.edges_visible = np.random.random(len(hex_grid_ints.edges)) > 0.5
+layer = viewer.add_graph(
+    hex_grid_ints, size=1, face_color='pink', edges_visible=False
+)
+# layer.edges_visible = np.random.random(len(hex_grid_ints.edges)) > 0.5
 
 if __name__ == "__main__":
     napari.run()
